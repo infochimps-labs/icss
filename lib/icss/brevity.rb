b9fb4870bd99d2c657f3eb2d849dd97033b911f4
Philip (flip) Kromer <flip@infochimps.org>
2011-03-04 18:18:30 -0600

    Include icss/brevity to get a compact representation for #inspect

---
+++ lib/icss/brevity.rb
@@ -0,0 +1,61 @@
+#
+# Doing
+#
+#     require 'icss/brevity'
+#
+# makes the #inspect method on Icss::Type's be nice and readable, not GIGANTE
+# PIQUANTE OY CABRON
+#
+#
+module Icss
+  Type.class_eval do
+    def inspect_with_brevity
+      ["#<#{self.class.name}",
+        inspect_hsh.map{|k,v| "#{k}=#{v}" },
+        ">",
+      ].join(" ")
+    end
+
+  private
+    def inspect_hsh
+      {
+        :name        => name,
+        :type        => type,
+        :doc         => "'#{(doc||"")[0..30].gsub(/[\n\t\r]+/,' ')}...'",
+    }
+    end
+  end
+
+  RecordType.class_eval do
+    private
+    # stuff a compact cartoon of the fields in there
+    def inspect_hsh
+      super.merge(
+        :fields_sum => (fields||[]).inject({}){|h,f| h[f.name] = f.type ; h }.inspect
+        )
+    end
+  end
+
+  Protocol.class_eval do
+    def inspect
+      ["#<#{self.class.name}",
+        inspect_hsh.map{|k,v| "#{k}=#{v}" },
+        ">"
+      ].join(" ")
+    end
+
+    def inspect_hsh
+      {
+        :name           => name,
+        :namespace      => namespace,
+        :types_br       => (types||[]).map(&:name).inspect,
+        :messages_br    => (messages||{}).values.map(&:name).inspect,
+        :data_assets_br => (data_assets||[]).map(&:name).inspect,
+        :doc            => "'#{(doc||"")[0..30].gsub(/[\n\t\r]+/,' ')}...'",
+      }
+    end
+  end
+
+
+end
+
