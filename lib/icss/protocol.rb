2d84e40ed2cd60a253ca061a7fd993b06c3d469c
Philip (flip) Kromer <flip@infochimps.org>
2011-03-05 23:07:40 -0600

    Documented everything in sight from the Avro specification. Starting to unfuck the RecordType / FieldType muddiness

--- lib/icss/protocol.rb
+++ lib/icss/protocol.rb
@@ -1,8 +1,61 @@
 module Icss
+
+  #
+  # Describes an Avro Protocol Declaration
+  #
+  # Avro protocols describe RPC interfaces. The Protocol class will receive an
+  # Avro JSON
+  #
+  # A Protocol has the following attributes:
+  #
+  # * protocol, a string, the name of the protocol (required). +name+ is
+  #   provided as an alias for +protocol+.
+  #
+  # * namespace, a string that qualifies the name (optional).
+  #
+  # * doc, a string describing this protocol (optional).
+  #
+  # * types, an optional list of definitions of named types (records, enums,
+  #   fixed and errors). An error definition is just like a record definition
+  #   except it uses "error" instead of "record". Note that forward references
+  #   to named types are not permitted.
+  #
+  # * messages, an optional JSON object whose keys are message names and whose
+  #   values are objects whose attributes are described below. No two messages
+  #   may have the same name.
+  #
+  # The name and namespace qualification rules defined for schema objects apply
+  # to protocols as well: see the documentation for Icss::Type.
+  #
+  # For example, one may define a simple HelloWorld protocol with:
+  #
+  #     {
+  #       "namespace":    "com.acme",
+  #       "protocol":     "HelloWorld",
+  #       "doc":          "Protocol Greetings",
+  #       "types": [
+  #         { "name":     "Greeting",
+  #           "type":     "record",
+  #           "fields":   [ {"name": "message", "type": "string"} ]},
+  #         { "name":     "Curse",
+  #           "type":     "error",
+  #           "fields":   [ {"name": "message", "type": "string"} ]}
+  #       ],
+  #       "messages": {
+  #         "hello": {
+  #           "doc":      "Say hello.",
+  #           "request":  [{"name": "greeting", "type": "Greeting" }],
+  #           "response": "Greeting",
+  #           "errors":   ["Curse"]
+  #         }
+  #       }
+  #     }
+  #
   class Protocol
     include Receiver
 
-    rcvr_accessor :name,        String
+    rcvr_accessor :protocol,    String, :required => true
+    alias_method  :name, :protocol
     rcvr_accessor :namespace,   String # must be *dotted* ("foo.bar"), not slashed ("foo/bar")
     rcvr_accessor :types,       Array, :of => Icss::TypeFactory
     rcvr_accessor :messages,    Hash,  :of => Icss::Message
@@ -10,16 +63,18 @@ module Icss
     rcvr_accessor :targets,     Hash,  :of => Icss::TargetListFactory
     rcvr_accessor :doc,         String
 
-    # String: namespace.name
-    def fullname
-      "#{namespace}.#{name}"
-    end
-
     # attr_accessor :body
     def after_receive hsh
-      self.name ||= hsh['protocol']
-      # self.body = hsh
+      # Set all the messages' protocol to self, and if the name wasn't given,
+      # set it using the messages hash key
       (self.messages||={}).each{|msg_name, msg| msg.protocol = self; msg.name ||= msg_name }
+      # Set all the type's parent to self (for namespace resolution)
+      (self.types   ||=[]).each{|type| type.parent  = self }
+    end
+
+    # String: namespace.name
+    def fullname
+      [namespace, name].compact.join(".")
     end
 
     def path
--- lib/icss/type.rb
+++ lib/icss/type.rb
@@ -3,6 +3,18 @@ module Icss
   # Generic ICSS base type
   #
 
+  # Avro Schema Declaration
+  #
+  # A Schema is represented in JSON by one of:
+  #
+  # * A JSON string, naming a defined type.
+  # * A JSON object, of the form:
+  #       {"type": "typeName" ...attributes...}
+  #   where typeName is either a primitive or derived type name, as defined
+  #   in the Icss::Type class
+  # * A JSON array, representing a union of embedded types.
+  #
+
   #
   # Here's the main thing you got to understand:
   #
@@ -13,19 +25,10 @@ module Icss
 
   class Type
     include Receiver
-    rcvr_accessor :name,    String
+    rcvr_accessor :name,    String, :required => true, :validates => :validate_avro_name
     rcvr_accessor :doc,     String
-    rcvr_accessor :type,    String
-    rcvr_accessor :default, Object # accept and love the object just as it is
-    # Type documentation
-    class_inheritable_accessor :name, :doc
     # Schema factory
     class_inheritable_accessor :ruby_klass, :pig_name
-
-    #
-    #
-    #
-    class OrderEnum < String ; end
 
     #
     # Factory methods
@@ -66,12 +69,328 @@ module Icss
 
   end
 
+  # ---------------------------------------------------------------------------
+  #
+  # Primitive Types
+  #
+
+  class NilClassType < Type
+    self.ruby_klass = NilClass
+    self.pig_name  = 'FIXME WHAT GOES HERE' # FIXME: ??
+  end
+  class BooleanType < Type
+    self.ruby_klass = Boolean
+    self.pig_name  = 'FIXME WHAT GOES HERE' # FIXME: ??
+  end
+  class IntegerType < Type
+    self.ruby_klass = Integer
+    self.pig_name  = 'int'
+  end
+  class LongType < Type
+    self.ruby_klass = Integer
+    self.pig_name  = 'long'
+  end
+  class FloatType < Type
+    self.ruby_klass = Float
+    self.pig_name  = 'float'
+  end
+  class DoubleType < Type
+    self.ruby_klass = Float
+    self.pig_name  = 'double'
+  end
+  class BytesType < Type
+    self.ruby_klass = String
+    self.pig_name  = 'bytearray'
+  end
+  class StringType < Type
+    self.ruby_klass = String
+    self.pig_name  = 'chararray'
+  end
+
+  # ---------------------------------------------------------------------------
+  #
+  # Complex Types
+  #
+
+  #
+  # Record, enums and fixed are named types. Each has a fullname that is
+  # composed of two parts; a name and a namespace. Equality of names is defined
+  # on the fullname.
+  #
+  # The name portion of a fullname, and record field names must:
+  # * start with [A-Za-z_]
+  # * subsequently contain only [A-Za-z0-9_]
+  # * A namespace is a dot-separated sequence of such names.
+  #
+  # References to previously defined names are as in the latter two cases above:
+  # if they contain a dot they are a fullname, if they do not contain a dot, the
+  # namespace is the namespace of the enclosing definition.
+  #
+  # Primitive type names have no namespace and their names may not be defined in
+  # any namespace. A schema may only contain multiple definitions of a fullname
+  # if the definitions are equivalent.
+  #
+  #
   class NamedType < Type
+    rcvr_accessor :namespace, String
+    attr_accessor :parent
+
     # def initialize name
     #   ::Icss::Type::DERIVED_TYPES[name.to_sym] = self
     # end
+
+    # In named types, the namespace and name are determined in one of the following ways:
+    #
+    # * A name and namespace are both specified. For example, one might use
+    #   "name": "X", "namespace": "org.foo" to indicate the fullname org.foo.X.
+    #
+    # * A fullname is specified. If the name specified contains a dot, then it is
+    #   assumed to be a fullname, and any namespace also specified is ignored. For
+    #   example, use "name": "org.foo.X" to indicate the fullname org.foo.X.
+    #
+    # * A name only is specified, i.e., a name that contains no dots. In this case
+    #   the namespace is taken from the most tightly enclosing schema or
+    #   protocol. For example, if "name": "X" is specified, and this occurs within
+    #   a field of the record definition of org.foo.Y, then the fullname is
+    #   org.foo.X.
+    #
+    def name= nm
+      if nm.include?('.')
+        split_name = nm.split('.')
+        @name      = split_name.pop
+        @namespace = split_name
+      else
+        @name = nm
+      end
+    end
+
+    # An avro name must
+    # * start with [A-Za-z_]
+    # * subsequently contain only [A-Za-z0-9_]
+    def validate_name nm
+      (nm =~ /\A[A-Za-z_]\w*\z/) or raise "An avro name must start with [A-Za-z_] and contain only [A-Za-z0-9_]. A namespace is the dot-separated sequence of such names."
+    end
+
+    def receive_namespace nmsp
+      # If the namespace is given in the name (using a dotted name string) then
+      # any namespace also specified is ignored.
+      if @namespace then warn "Warning: namespace already set, ignoring" and return ; end
+      @namespace = nmsp
+    end
+
+    def namespace
+      @namespace || parent.namespace
+    end
+
+    #
+    # If no explicit namespace is specified, the namespace is taken from the
+    # most tightly enclosing schema or protocol. For example, if "name": "X" is
+    # specified, and this occurs within a field of the record definition of
+    # org.foo.Y, then the fullname is org.foo.X.
+    #
+    def fullname
+      [namespace, name].join('.')
+    end
   end
 
+  #
+  # Describes a field in an Avro Record object.
+  #
+  # Each field has the following attributes:
+  # * name:     a string providing the name of the field (required), and
+  # * doc:      a string describing this field for users (optional).
+  # * type:     a Type object defining a schema, or a string or symbol naming a
+  #             record definition (required).
+  #
+  #             avro type     json type       ruby type       example
+  #             null          null            NilClass        nil
+  #             boolean       boolean         Boolean         true
+  #             int,long      integer         Integer         1
+  #             float,double  number          Float           1.1
+  #             bytes         string          String          "\u00FF"
+  #             string        string          String          "foo"
+  #             record        object          RecordType      {"a": 1}
+  #             enum          string          Enum            "FOO"
+  #             array         array           Array           [1]
+  #             map           object          Hash            { "a": 1 }
+  #             fixed         string          String          "\u00ff"
+  #
+  # * default:  a default value for this field, used when reading instances that
+  #             lack this field (optional). Permitted values depend on the
+  #             field's schema type, according to the table below. Default
+  #             values for union fields correspond to the first schema in the
+  #             union. Default values for bytes and fixed fields are JSON
+  #             strings, where Unicode code points 0-255 are mapped to unsigned
+  #             8-bit byte values 0-255.
+  #
+  # * order:    specifies how this field impacts sort ordering of this record
+  #             (optional). Valid values are "ascending" (the default),
+  #             "descending", or "ignore". For more details on how this is used,
+  #             see the the sort order section below.
+  #
+  # See RecordType for examples.
+  #
+  class RecordField
+    rcvr_accessor :name,      String, :required => true
+    rcvr_accessor :doc,       String
+    rcvr_accessor :type,      String, :required => true
+    rcvr_accessor :default,   Object # accept and love the object just as it is
+    rcvr          :order,     String
+
+    ALLOWED_ORDERS = %w[ascending descending ignore]
+    def order
+      @order || 'ascending'
+    end
+    def order_direction
+      case order when 'ascending' then 1 when 'descending' then -1 else 0 ; end
+    end
+    # QUESTION: should this be an override of order=, or of receive_order?
+    def order= v
+      raise "'order' may only take the values ascending (the default), descending, or ignore." unless v.nil? || ALLOWED_ORDERS.include?(v)
+      self.order = v
+    end
+  end
+
+  #
+  # Icss/Avro Record type
+  #
+  # Records use the type name "record" and support these attributes:
+  #
+  # * name:      a string providing the name of the record (required).
+  # * namespace: a string that qualifies the name;
+  # * doc:       a string providing documentation to the user of this schema (optional).
+  # * fields:    an array of RecordField's (required).
+  #
+  # For example, a linked-list of 64-bit values may be defined with:
+  #
+  #         {
+  #           "type": "record",
+  #           "name": "LongList",
+  #           "fields" : [
+  #             {"name": "value", "type": "long"},             // each element has a long
+  #             {"name": "next", "type": ["LongList", "null"]} // optional next element
+  #           ]
+  #         }
+  #
+  class RecordType < NamedType
+    rcvr_accessor :fields, Array, :of => Icss::TypeFactory, :required => true
+
+    # def ruby_klass
+    #   @klass ||= Class.new do
+    #     fields.each do |field|
+    #       instance_eval{ field.define_receiver }
+    #     end
+    #   end
+    # end
+
+    #
+    # Conversion
+    #
+    def to_hash
+      super.merge( :fields => (fields||[]).map{|field| field.to_hash} )
+    end
+  end
+
+  #  An error definition is just like a record definition except it uses "error" instead of "record".
+  class ErrorType < RecordType
+  end
+
+  #
+  # Describes an Avro Enum type.
+  #
+  # Enums use the type name "enum" and support the following attributes:
+  #
+  # name:       a string providing the name of the enum (required).
+  # namespace:  a string that qualifies the name;
+  # doc:        a string providing documentation to the user of this schema (optional).
+  # symbols:    an array, listing symbols, as strings or ruby symbols (required). All
+  #             symbols in an enum must be unique; duplicates are prohibited.
+  #
+  # For example, playing card suits might be defined with:
+  #
+  # { "type": "enum",
+  #   "name": "Suit",
+  #   "symbols" : ["SPADES", "HEARTS", "DIAMONDS", "CLUBS"]
+  # }
+  #
+  class EnumType < NamedType
+    rcvr_accessor :symbols, Array, :of => Symbol, :required => true
+  end
+
+  #
+  # ArrayType describes an Avro Array type.
+  #
+  # Arrays use the type name "array" and support a single attribute:
+  #
+  # * items: the schema of the array's items.
+  #
+  # @example, an array of strings is declared with:
+  #
+  #     {"type": "array", "items": "string"}
+  #
+  class ArrayType < Type
+    # FIXME: is items required? The schema doesn't say so.
+    rcvr_accessor :items, TypeFactory
+  end
+
+  #
+  # MapType describes an Avro Map type (which corresponds to a Ruby
+  # Hash). HashType is a synonym for MapType.
+  #
+  # Maps use the type name "map" and support one attribute:
+  #
+  # * values: the schema of the map's values. Avro Map keys are assumed to be strings.
+  #
+  # @example, a map from string to long is declared with:
+  #
+  #     {"type": "map", "values": "long"}
+  #
+  class MapType < Type
+    # FIXME: is items required? The schema doesn't say so.
+    rcvr_accessor :values, TypeFactory
+  end
+  HashType = MapType
+
+  #
+  # Describes an Avro Union type.
+  #
+  # Unions are represented using JSON arrays. For example, ["string", "null"]
+  # declares a schema which may be either a string or null.
+  #
+  # Unions may not contain more than one schema with the same type, except for
+  # the named types record, fixed and enum. For example, unions containing two
+  # array types or two map types are not permitted, but two types with different
+  # names are permitted. (Names permit efficient resolution when reading and
+  # writing unions.)
+  #
+  # Unions may not immediately contain other unions.
+  #
+  class UnionType < Type
+    def receive *args
+      raise "Not implemented yet"
+    end
+  end
+
+  #
+  # Describes an Avro Fixed type.
+  #
+  # Fixed uses the type name "fixed" and supports the attributes:
+  #
+  # * name: a string naming this fixed (required).
+  # * namespace, a string that qualifies the name;
+  # * size: an integer, specifying the number of bytes per value (required).
+  #
+  #   For example, 16-byte quantity may be declared with:
+  #
+  #     {"type": "fixed", "size": 16, "name": "md5"}
+  #
+  class FixedType < Type
+    rcvr_accessor :size, Integer, :required => true
+  end
+
+  #
+  #
+  #
   class TypeFactory < Type
     # I feel like the case statement below could be improved...
     def self.receive hsh
@@ -100,73 +419,6 @@ module Icss
       klass.type = :record
       ::Icss::Type::DERIVED_TYPES[hsh[:name].to_sym] = klass
     end
-  end
-
-  #
-  # Icss/Avro Record type
-  #
-  # A record type has fields, each of which
-  #
-  class RecordType < NamedType
-    rcvr_accessor :fields, Array, :of => Icss::TypeFactory
-    rcvr_accessor :order,  OrderEnum
-    class_inheritable_accessor :doc, :type
-
-    def ruby_klass
-      @klass ||= Class.new do
-        fields.each do |field|
-          rcvr_accessor field.name, field.ruby_klass
-        end
-      end
-    end
-
-    def self.to_hash
-      { :name => name, :doc => doc }
-    end
-
-    #
-    # Conversion
-    #
-    def to_hash
-      super.merge( :fields => (fields||[]).map{|field| field.to_hash} )
-    end
-  end
-
-  class NilClassType < Type
-    self.ruby_klass = NilClass
-    self.pig_name  = 'FIXME WHAT GOES HERE' # FIXME: ??
-  end
-  class StringType < Type
-    self.ruby_klass = String
-    self.pig_name  = 'chararray'
-  end
-  class IntegerType < Type
-    self.ruby_klass = Integer
-    self.pig_name  = 'int'
-  end
-  class BooleanType < Type
-    self.ruby_klass = Boolean
-    self.pig_name  = 'FIXME WHAT GOES HERE' # FIXME: ??
-  end
-  class LongType < Type
-    self.ruby_klass = Integer
-    self.pig_name  = 'long'
-  end
-  class FloatType < Type
-    self.ruby_klass = Float
-    self.pig_name  = 'float'
-  end
-  class DoubleType < Type
-    self.ruby_klass = Float
-    self.pig_name  = 'double'
-  end
-  class BytesType < Type
-    self.ruby_klass = String
-    self.pig_name  = 'bytearray'
-  end
-  class FixedType < Type
-    self.ruby_klass = String
-    self.pig_name  = 'bytearray'
   end
 
   Type.class_eval do
