aea02009e1ce6446c24c49cae27ba15b041bc1ae
Philip (flip) Kromer <flip@infochimps.org>
2011-03-07 04:26:21 +0000

    fake hash accessor semantics

--- lib/icss/receiver/extensions.rb
+++ lib/icss/receiver/extensions.rb
@@ -1,5 +1,8 @@
 module Receiver
 
+  #
+  # Makes a Receiver thingie behave mostly like a hash.
+  #
   module ActsAsHash
     def to_hash
       self.class.receiver_attr_names.inject({}) do |h,name|
@@ -7,6 +10,23 @@ module Receiver
         h[name] = (val.respond_to?(:to_hash) ? val.to_hash : val) if val
         h
       end
+    end
+
+    # Fake hash reader semantics
+    #
+    # Note: indifferent access -- either of :foo or "foo" will work
+    def [](name)
+      self.send(name)
+    end
+
+    # Fake hash writer semantics
+    #
+    # NOTE: this calls self.foo= 5, not self.receive_foo(5) --
+    #   only one layer of sugar at a time, sweetie.
+    #
+    # Note: indifferent access -- either of :foo or "foo" will work
+    def []=(name, val)
+      self.send("#{name}=", val)
     end
   end
 
