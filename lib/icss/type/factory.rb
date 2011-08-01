3f32375e6e98912b6041fce5748d5c247633fcba
Philip (flip) Kromer <flip@infochimps.org>
2011-08-01 06:22:18 -0500

    moved base class cruft out of the top of the file

--- lib/icss/type/factory.rb
+++ lib/icss/type/factory.rb
@@ -1,4 +1,90 @@
 module Icss
+  class RecordType < NamedType
+
+    def ruby_klass_scope_names
+      fullname.split('.').map(&:camelize)
+    end
+    def ruby_klass_name
+      "::" + ruby_klass_scope_names.join('::')
+    end
+    def ruby_klass_parent
+      ("::" + ruby_klass_scope_names[0..-2].join('::')).constantize
+    end
+
+    def ensure_parent_modules!
+      ruby_klass_scope_names[0..-2].inject(Object) do |parent_module, module_name|
+        new_parent   = "::#{parent_module}::#{module_name}".constantize rescue nil
+        new_parent ||= parent_module.const_set(module_name.to_sym, Module.new)
+        # p [parent_module, new_parent, module_name]
+        new_parent
+      end
+    end
+
+    def define_klass
+      ensure_parent_modules!
+      parent_module = ruby_klass_parent
+      klass_basename = ruby_klass_scope_names.last.to_sym
+      klass   = parent_module.const_get(klass_basename) rescue nil
+      klass ||= parent_module.const_set(klass_basename, Class.new(::Icss::MetaType))
+      klass
+    end
+
+    def ruby_klass
+      return @klass if @klass
+      klass = define_klass
+      decorate_with_receivers(klass)
+      decorate_with_conveniences(klass)
+      decorate_with_validators(klass)
+      @klass = klass
+    end
+
+    def decorate_with_receivers klass
+      fields.each do |field|
+        field.define_receiver_on(klass)
+      end
+    end
+
+    def decorate_with_conveniences klass
+      klass.send :include, Receiver::ActsAsHash
+      klass.send :include, Receiver::ActiveModelShim
+    end
+
+    def decorate_with_validators klass
+      fields.each do |field|
+        puts field.to_hash
+        if field.validates
+          puts field.validates
+          klass.validates(field.name.to_sym, field.validates)
+        end
+      end
+    end
+
+  end
+
+  Icss::RecordField.class_eval do
+    def define_receiver_on(klass)
+      if union? || enum?
+        warn "Can't handle union or enum types yet: #{self.inspect}"
+        return
+      end
+      p self if type.nil?
+      klass.rcvr_accessor name.to_sym, type.ruby_klass, field_receiver_attrs
+    end
+
+    def field_receiver_attrs
+      attrs = {}
+      (self.class.receiver_attr_names - [:name, :type]).each do |attr|
+        val = attr_set?(attr) && self.send(attr) or next
+        attrs[attr] = val
+      end
+      case self.type
+      when Icss::ArrayType then attrs[:of] = self.type.items.ruby_klass
+      end
+      attrs
+    end
+  end
+
+
   class MetaType
     include Receiver
 
@@ -143,88 +229,5 @@ module Icss
     rcvr_accessor :properties, Hash
   end
 
-  class RecordType < NamedType
 
-    def ruby_klass_scope_names
-      fullname.split('.').map(&:camelize)
-    end
-    def ruby_klass_name
-      "::" + ruby_klass_scope_names.join('::')
-    end
-    def ruby_klass_parent
-      ("::" + ruby_klass_scope_names[0..-2].join('::')).constantize
-    end
-
-    def ensure_parent_modules!
-      ruby_klass_scope_names[0..-2].inject(Object) do |parent_module, module_name|
-        new_parent   = "::#{parent_module}::#{module_name}".constantize rescue nil
-        new_parent ||= parent_module.const_set(module_name.to_sym, Module.new)
-        # p [parent_module, new_parent, module_name]
-        new_parent
-      end
-    end
-
-    def define_klass
-      ensure_parent_modules!
-      parent_module = ruby_klass_parent
-      klass_basename = ruby_klass_scope_names.last.to_sym
-      klass   = parent_module.const_get(klass_basename) rescue nil
-      klass ||= parent_module.const_set(klass_basename, Class.new(::Icss::MetaType))
-      klass
-    end
-
-    def ruby_klass
-      return @klass if @klass
-      klass = define_klass
-      decorate_with_receivers(klass)
-      decorate_with_conveniences(klass)
-      decorate_with_validators(klass)
-      @klass = klass
-    end
-
-    def decorate_with_receivers klass
-      fields.each do |field|
-        field.define_receiver_on(klass)
-      end
-    end
-
-    def decorate_with_conveniences klass
-      klass.send :include, Receiver::ActsAsHash
-      klass.send :include, Receiver::ActiveModelShim
-    end
-
-    def decorate_with_validators klass
-      fields.each do |field|
-        puts field.to_hash
-        if field.validates
-          puts field.validates
-          klass.validates(field.name.to_sym, field.validates)
-        end
-      end
-    end
-
-  end
-
-  Icss::RecordField.class_eval do
-    def define_receiver_on(klass)
-      if union? || enum?
-        warn "Can't handle union or enum types yet: #{self.inspect}"
-        return
-      end
-      p self if type.nil?
-      klass.rcvr_accessor name.to_sym, type.ruby_klass, field_receiver_attrs
-    end
-
-    def field_receiver_attrs
-      attrs = {}
-      (self.class.receiver_attr_names - [:name, :type]).each do |attr|
-        val = attr_set?(attr) && self.send(attr) or next
-        attrs[attr] = val
-      end
-      case self.type
-      when Icss::ArrayType then attrs[:of] = self.type.items.ruby_klass
-      end
-      attrs
-    end
-  end
 end
