f1d547ae315a04a14d35c2eab8b12e43c38e676a
Philip (flip) Kromer <flip@infochimps.org>
2011-08-23 00:58:27 -0500

    make subclasses of 'Thing' be receiver_models

---
+++ lib/icss/core_types.rb
@@ -0,0 +1,9 @@
+Icss::Meta::Protocol.load_from_catalog('core/*')
+
+if defined?(Icss::Thing)
+  Icss::Thing.class_eval do
+    include Icss::ReceiverModel
+  end
+else
+  warn 'Could not load core type "thing". Make sure the catalog is where you expect it to be.'
+end
