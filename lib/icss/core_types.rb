54102303a6ad5c42bde108b3cee4608ab1c146dd
Philip (flip) Kromer <flip@infochimps.org>
2011-08-26 05:46:41 -0500

    getting new types to display

--- lib/icss/core_types.rb
+++ lib/icss/core_types.rb
@@ -1,6 +1,7 @@
 # p [__FILE__, "core types from ", Settings.catalog_root]
 
 module Icss
+  module Encyclopedic ; module Wikipedia ; module Dbpedia ; end ; end ; end
   module Web ; module An ; end ; end
   module Engineering ; module Chemical; module Msds ; end ; end ; end
   module Government ; module Public ; module Acs ; end ; end ; end
@@ -14,7 +15,9 @@ end
 
 Icss::Meta::Protocol.load_from_catalog('core/thing')
 Icss::Meta::Protocol.load_from_catalog('core/*')
+Icss::Meta::Protocol.load_from_catalog('core/geo/place')
 # Icss::Meta::Protocol.load_from_catalog('core/**/*')
+Icss::Meta::Protocol.load_from_catalog('datasets/encyclopedic/wikipedia/wikipedia_article')
 
 unless defined?(Icss::Geo::Place)
   warn "Could not load core type 'place'. Make sure the catalog (#{Settings.catalog_root}) is where you expect it to be."
